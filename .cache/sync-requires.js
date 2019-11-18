22fca63aff355ac5b6ca04832e89deae3938a16c
iyaja <ajayuppili@gmail.com>
2019-11-17 19:46:27 -0600

    chore: run deploy script

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,6 +5,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/ajayarasanipalai/IllinoisRoboticsInSpace.github.io/src/pages/index.js")))
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/ajayarasanipalai/iris-website/src/pages/index.js")))
 }
 
