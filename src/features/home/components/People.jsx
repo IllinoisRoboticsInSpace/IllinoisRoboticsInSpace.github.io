51dfbf839db71c2417306ea76c6a5284d7f2749c
iyaja <ajayuppili@gmail.com>
2019-12-01 12:05:24 -0600

    feat: add containers for board members and titles

--- src/features/home/components/People.jsx
+++ src/features/home/components/People.jsx
@@ -8,9 +8,20 @@ const People = (props, ref) => {
 		<div ref={ref} className="peopleSection sectionContainer">
 			<h1>People</h1>
 			<div className="peopleList">
-				<Person name="Madara Uchiha" position="Caster of the infinite tsukuyomi" imageSrc="/people/madara.png" />
-				<Person name="Hagoromo Ōtsutsuki" position="Sage of the six paths" imageSrc="/people/hagoromo.png" />
-				<Person name="Naruto Uzumaki" position="Seventh Hokage of the hidden leaf and a whole lot of other things" imageSrc="/people/naruto.png" />
+				<Person name="Victoria Jiron" position="President" imageSrc="/people/victoria.jpg" />
+				<Person name="Liz Boehning" position="Vice President" imageSrc="/people/liz.jpg" />
+				<Person name="Lukas Adomaviciute" position="Secretary" imageSrc="/people/null.jpg" />
+				<Person name="Patrick Kulach" position="Treasurer" imageSrc="/people/null.jpg" />
+				<Person name="Jake Nickel" position="Project Manager and Electrical Lead" imageSrc="/people/null.jpg" />
+				<Person name="Gavin Chan" position="Publicity Chair" imageSrc="/people/gavin.jpg" />
+				<Person name="Aryan Dawra" position="Outreach Chair" imageSrc="/people/null.jpg" />
+				<Person name="Ethan Hoggard" position="Systems Team Lead" imageSrc="/people/null.jpg" />
+				<Person name="Ananya Yammanuru" position="Auto Team Lead" imageSrc="/people/null.jpg" />
+				<Person name="Peter Gutfeldt" position="Mechanical Team Lead" imageSrc="/people/null.jpg" />
+				<Person name="Elizabeth Karlovics" position="Social Media Chair" imageSrc="/people/null.jpg" />
+				<Person name="Nate Claussen" position="Corporate Outreach" imageSrc="/people/null.jpg" />
+				<Person name="Aditya Gupta" position="Webmaster" imageSrc="/people/null.jpg" />
+				<Person name="Ajay Arasanipalai" position="Webmaster" imageSrc="/people/null.jpg" />
 			</div>
 		</div>
 	);
