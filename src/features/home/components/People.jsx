047dcad3fdbf1b304ce1f64899d0dc015058c1d7
iyaja <ajayuppili@gmail.com>
2019-12-08 19:31:46 -0600

    feat: add some board pictures

--- src/features/home/components/People.jsx
+++ src/features/home/components/People.jsx
@@ -10,18 +10,20 @@ const People = (props, ref) => {
 			<div className="peopleList">
 				<Person name="Victoria Jiron" position="President" imageSrc="/people/victoria.jpg" />
 				<Person name="Liz Boehning" position="Vice President" imageSrc="/people/liz.jpg" />
-				<Person name="Lukas Adomaviciute" position="Secretary" imageSrc="/people/null.jpg" />
-				<Person name="Patrick Kulach" position="Treasurer" imageSrc="/people/null.jpg" />
-				<Person name="Jake Nickel" position="Project Manager and Electrical Lead" imageSrc="/people/null.jpg" />
+				<Person name="Lukas Adomaviciute" position="Secretary" imageSrc="/people/lukas.jpg" />
+				<Person name="Patrick Kulach" position="Treasurer" imageSrc="/people/patrick.jpg" />
+				<Person name="Jake Nickel" position="Project Manager and Electrical Lead" imageSrc="/people/jake.jpg" />
 				<Person name="Gavin Chan" position="Publicity Chair" imageSrc="/people/gavin.jpg" />
-				<Person name="Aryan Dawra" position="Outreach Chair" imageSrc="/people/null.jpg" />
-				<Person name="Ethan Hoggard" position="Systems Team Lead" imageSrc="/people/null.jpg" />
-				<Person name="Ananya Yammanuru" position="Auto Team Lead" imageSrc="/people/null.jpg" />
-				<Person name="Peter Gutfeldt" position="Mechanical Team Lead" imageSrc="/people/null.jpg" />
-				<Person name="Elizabeth Karlovics" position="Social Media Chair" imageSrc="/people/null.jpg" />
-				<Person name="Nate Claussen" position="Corporate Outreach" imageSrc="/people/null.jpg" />
-				<Person name="Aditya Gupta" position="Webmaster" imageSrc="/people/null.jpg" />
-				<Person name="Ajay Arasanipalai" position="Webmaster" imageSrc="/people/null.jpg" />
+				<Person name="Megan Shapland" position="Corporate Outreach" imageSrc="/people/megan.jpg" />
+				<Person name="Aryan Dawra" position="Outreach Chair" imageSrc="/people/aryan.jpg" />
+				<Person name="Christian Held" position="Electrical Consultant" imageSrc="/people/christian.jpg" />
+				<Person name="Ethan Hoggard" position="Systems Team Lead" imageSrc="/people/ethan.jpg" />
+				<Person name="Ananya Yammanuru" position="Auto Team Lead" imageSrc="/people/ananya.jpg" />
+				<Person name="Peter Gutfeldt" position="Mechanical Team Lead" imageSrc="/people/peter.jpg" />
+				<Person name="Elizabeth Karlovics" position="Social Media Chair" imageSrc="/people/elizabeth.jpg" />
+				<Person name="Position Open for Applications" position="Corporate Outreach" imageSrc="/people/nate.jpg" />
+				<Person name="Aditya Gupta" position="Webmaster" imageSrc="/people/adithya.jpg" />
+				<Person name="Ajay Arasanipalai" position="Webmaster" imageSrc="/people/ajay.jpg" />
 			</div>
 		</div>
 	);
